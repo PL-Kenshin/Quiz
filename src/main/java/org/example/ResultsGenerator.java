197ecf378b7d7427d0ff9e2557f3db46805b6a0b
PL-Kenshin <78429627+PL-Kenshin@users.noreply.github.com>
2022-06-17 23:10:18 +0200

    Hotfix paths

--- src/main/java/org/example/ResultsGenerator.java
+++ src/main/java/org/example/ResultsGenerator.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ResultsGenerator {
 
-    private static final String fileName = "C:\\Users\\Admin\\Desktop\\Programming\\JAVA\\quiz\\src\\main\\resources\\org\\example\\userResults.csv";
+    private static final String fileName = System.getProperty("user.dir") + "/src/main/resources/org/example/userResults.csv";
     private static List<UserAnswer> userAnswerList;
 
     /**
